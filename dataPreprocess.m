e1e85a317c640fc11c5b05ebfb377a92e8a15041
roynirmal <nroy000@gmail.com>
2018-01-12 16:38:14 +0100

    preprocesses all data now

--- dataPreprocess.m
+++ dataPreprocess.m
@@ -1,6 +1,6 @@
 function a = dataPreprocess(resizeSize, resizeMethod)
 
-a = prnist([0:9],[1:40:1000]);
+a = prnist([0:9],[1:1:1000]);
 
 preproc = im_box([],0,1)*im_resize([],[resizeSize resizeSize],resizeMethod)*im_box([],1,0); %resize method needs tuning
 a = a*preproc;
