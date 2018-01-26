d8b84363ae245888509205ecef9165ac25360d61
roynirmal <nroy000@gmail.com>
2018-01-26 10:56:36 +0100

    barbara use this

--- featurelearningcurve.m
+++ featurelearningcurve.m
@@ -1,6 +1,6 @@
  a = dataPreprocess(16, 'bicubic');
  pr_ds_features = featCoding(a, 16, true, false);
-  [sel,r] = featselp(pr_ds_features,'maha-s',100);
+  [sel,r] = featselp(pr_ds_features,'maha-s',50);
     
 % 
  trdata= [200,250, 300, 350, 400, 450, 500,550,  600, 650, 700, 750,800, 850, 900];
@@ -8,17 +8,17 @@
 %trdata = 600;
  for i = 1: length(trdata)
      temperr = 0 ;
-     for j = 1:10
-     [trn,tst] = gendat(pr_ds_features*sel,nrTrObjectsPerClass/1000);
-      [w, n] = pcam(trn,0.95);
- pcaTrained=scalem(trn,'variance')*w;
+     for j = 1:1
+     [trn,tst] = gendat(pr_ds_features*sel,trdata(i)/1000);
+      [ww, n] = pcam(trn,0.95);
+ pcaTrained=scalem(trn,'variance')*ww;
 %pcaTrained=trn*sel;
 trn = trn*pcaTrained;
   tst = tst*pcaTrained;
-  
+%   
 %   f = parzenc(trn);
 %   errc= testc(tst,f);
-    f = fisherc(trn);
+    f = svc(trn);
   errc= testc(tst,f);
   temperr = temperr + errc;
      end    
